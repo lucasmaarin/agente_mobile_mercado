32d96ec4e290706e54d7bd4b653efd4c0cc31023
Lucas Marin <josemarin.nj@gmail.com>
2026-02-13 11:26:15 -0300

    Teste3

--- src/index.js
+++ src/index.js
@@ -36,7 +36,8 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production',
+        secure: false,
+        httpOnly: true,
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dias
         sameSite: 'lax'
     }
